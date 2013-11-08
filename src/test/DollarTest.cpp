60b37e64a94fc3c27a676adfb8b9a9e2fda52e9c
qiubix <qiubix@gmail.com>
2013-11-08 17:34:04 +0100

    Checking assertion types in gtest.

--- src/test/DollarTest.cpp
+++ src/test/DollarTest.cpp
@@ -22,9 +22,16 @@ TEST_F(DollarTest, testMultiplication) {
 }
 
 
+TEST_F(DollarTest, testMultiplication1) {
+    Dollar* five = new Dollar(5);
+    five -> times(2);
+
+    ASSERT_TRUE(10 == five->amount);
+}
+
 TEST_F(DollarTest, testMultiplication2) {
     Dollar* six = new Dollar(6);
     six -> times(2);
 
-    EXPECT_THAT(12, Eq(six -> amount));
+    ASSERT_EQ(12, six->amount);
 }
