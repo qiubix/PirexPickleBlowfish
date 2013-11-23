32097088455f51ed0e7e7ca56a0f3cc4a33cfd29
qiubix <qiubix@gmail.com>
2013-11-23 13:42:15 +0100

    fixed indentation

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,20 @@
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    //Karol właśnie dodał nową linię i się z tego bardzo cieszy.
-    //Artur też chce!
-    //nowy system test
-    return 0;
+  std::cout << "Hello World!" << std::endl;
+  //Karol właśnie dodał nową linię i się z tego bardzo cieszy.
+  //Artur też chce!
+  //nowy system test
+  return 0;
 }
 
-int firstFunction(int argument)
+int firstFunction (int argument)
 {
-    if (argument > 0) {
-        argument = 0;
-    }
-    else {
-        argument = 1;
-    }
-    return argument;
+  if (argument > 0) {
+    argument = 0;
+  }
+  else {
+    argument = 1;
+  }
+  return argument;
 }
