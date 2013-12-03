f025ae39bf54a0319a7476fc40ce12acf59dbe32
qiubix <qiubix@gmail.com>
2013-12-03 19:15:05 +0100

    Using Dollar and BoardSample in main()
    Added some instances of Dollar and BoardSample to check if it runs.
    Fucking nailed it, finally Qt5 GUI works and it seems that I know how to
    make this cmake build and link properly.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-#include "GUI/BoardSample.hpp"
+#include "Dollar.hpp"
+#include "BoardSample.hpp"
+//#include "GUI/BoardSample.hpp"
 //#include <QApplication>
 
 int main(int argc, char* argv[])
 {
-//  QApplication app(argc, argv);
-//  BoardSample boardSample;
-//  boardSample.show();
+  QApplication app(argc, argv);
+  BoardSample boardSample;
+  boardSample.show();
+  Dollar* dollar = new Dollar(5);
+  dollar->times(2);
   std::cout << "Hello World!" << std::endl;
-//  return app.exec();
-  return 0;
+  return app.exec();
+//  return 0;
 }
