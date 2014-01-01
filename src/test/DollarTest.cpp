d09a4d14a1c6ac67d80ffc490e9d7d12a3ee548b
owca <artus.sawicki@gmail.com>
2014-01-01 15:47:17 +0100

    test examples read and fixed

--- src/test/DollarTest.cpp
+++ src/test/DollarTest.cpp
@@ -11,9 +11,9 @@ protected:
   ~DollarTest(){}
 
   virtual void SetUp() {
-    Dollar* five = new Dollar(5);
+    five = new Dollar(5);
     five -> times(2);
-    Dollar* six = new Dollar(6);
+    six = new Dollar(6);
     six -> times(2);
   }
   virtual void TearDown() {
@@ -24,29 +24,18 @@ protected:
   Dollar* six;
 };
 
-// Zwykly test. Nonfatal assertion
-TEST(DollarTest, testMultiplication) {
+TEST(DollarNotFixtureTest, testMultiplication) {
   Dollar* five = new Dollar(5);
   five -> times(2);
   EXPECT_THAT(10, Eq(five->amount));
+  delete five;
 }
 
-// Test fixture. Używa wspólnych danych z innymi testami, zainicjowanymi
-// w SetUp(). Musi się nazywać TEST_F. Fatal assertion
 TEST_F(DollarTest, testMultiplication1) {
   ASSERT_TRUE(10 == five->amount);
 }
 
-// Test z nonfatal i fatal assertions
 TEST_F(DollarTest, testMultiplication2) {
-  EXPECT_EQ(12, six->amount);
-  ASSERT_EQ(10, five->amount);
+  ASSERT_EQ(12, six->amount);
+  ASSERT_EQ(12, five->amount);
 }
-
-/* TODO: Komentarze do wywalenia po przeczytaniu
- * Odpalenie tego testu będzie wywalać warningi/błędy,
- * bo generalnie nie można używać w jednym test casie
- * TEST() i TEST_F() obok siebie. W tym przypadku wszystkie
- * powinne być TEST_F, bo w setupie jest już przeprowadzona
- * inicjacja.
- */
