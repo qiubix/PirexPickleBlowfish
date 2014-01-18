d2970f6a5ecc74ae247058de5fd66b4e7cc5401e
qiubix <qiubix@gmail.com>
2014-01-18 22:14:35 +0100

    Add empty concrete instant token classes
    derived from InstantToken class, implementing virtual method action().

--- src/logic/InstantToken.cpp
+++ src/logic/InstantToken.cpp
@@ -3,3 +3,57 @@
 
 InstantToken::InstantToken(Army army, std::string name)
   : Token(army, name) {}
+
+
+BattleToken::BattleToken(Army army, std::string name)
+  : InstantToken(army, name) {}
+
+void BattleToken::action()
+{
+
+}
+
+
+MovementToken::MovementToken(Army army, std::string name)
+  : InstantToken(army, name) {}
+
+void MovementToken::action()
+{
+
+}
+
+
+PushToken::PushToken(Army army, std::string name)
+  : InstantToken(army, name) {}
+
+void PushToken::action()
+{
+
+}
+
+
+BombToken::BombToken(Army army, std::string name)
+  : InstantToken(army, name) {}
+
+void BombToken::action()
+{
+
+}
+
+
+GranadeToken::GranadeToken(Army army, std::string name)
+  : InstantToken(army, name) {}
+
+void GranadeToken::action()
+{
+
+}
+
+
+SniperToken::SniperToken(Army army, std::string name)
+  : InstantToken(army, name) {}
+
+void SniperToken::action()
+{
+
+}
--- src/logic/InstantToken.hpp
+++ src/logic/InstantToken.hpp
@@ -12,4 +12,58 @@ public:
   virtual void action(void) = 0;
 };
 
+class BattleToken : public InstantToken
+{
+public:
+  BattleToken(Army army, std::string name = "Battle");
+  ~BattleToken(void) {}
+
+  void action(void);
+};
+
+class MovementToken : public InstantToken
+{
+public:
+  MovementToken(Army army, std::string name = "Movement");
+  ~MovementToken() {}
+
+  void action(void);
+};
+
+class PushToken : public InstantToken
+{
+public:
+  PushToken(Army army, std::string name = "Push");
+  ~PushToken() {}
+
+  void action(void);
+};
+
+class BombToken : public InstantToken
+{
+public:
+  BombToken(Army army, std::string name = "Bomb");
+  ~BombToken() {}
+
+  void action(void);
+};
+
+class GranadeToken : public InstantToken
+{
+public:
+  GranadeToken(Army army, std::string name = "Granade");
+  ~GranadeToken() {}
+
+  void action(void);
+};
+
+class SniperToken : public InstantToken
+{
+public:
+  SniperToken(Army army, std::string name = "Sniper");
+  ~SniperToken() {}
+
+  void action(void);
+};
+
 #endif //INSTANTTOKEN_HPP
