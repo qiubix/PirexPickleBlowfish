082e459497e735ab5f097932ed8890e152579a9f
owca <artus.sawicki@gmail.com>
2014-01-26 15:02:46 +0100

    drawing hexagon

--- src/gui/Field.cpp
+++ src/gui/Field.cpp
@@ -1,4 +1,5 @@
 #include "Field.hpp"
+#include <QtCore/qmath.h>
 
 Field::Field(QGraphicsItem *parent) : QObject(), QGraphicsItem(parent)
 {
@@ -8,8 +9,18 @@ Field::Field(QGraphicsItem *parent) : QObject(), QGraphicsItem(parent)
 void Field::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
   Q_UNUSED(option)
   Q_UNUSED(widget)
-  painter -> setBrush(Qt::yellow);
-  painter -> drawEllipse(-width/2, -width/2, width, width);
+  painter -> setBrush(Qt::red);
+
+  float d = 50.0;
+
+  QPolygonF polygon;
+  polygon << QPoint(-d, 0);
+  polygon << QPoint(-d/2, qSqrt(3)*d/2);
+  polygon << QPoint(d/2, qSqrt(3)*d/2);
+  polygon << QPoint(d, 0);
+  polygon << QPoint(d/2, -qSqrt(3)*d/2);
+  polygon << QPoint(-d/2, -qSqrt(3)*d/2);
+  painter -> drawPolygon(polygon);
 }
 
 QRectF Field::boundingRect() const {
