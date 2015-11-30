d8e4bd7d6e71d0dca187dc5379e35d4b6c72b9da
qiubix <qiubix@gmail.com>
2015-11-30 10:49:25 +0100

    List test cases

--- test/ConsoleUITest.cpp
+++ test/ConsoleUITest.cpp
@@ -8,3 +8,14 @@ using ::testing::Test;
 TEST(ConsoleUITest, shouldTestNothing) {
   ASSERT_THAT(1, Eq(1));
 }
+
+
+/*
+ * should indicate empty board at the beginning
+ * should inform player about possible actions
+ * should allow to display current state of the board
+ * should allow to draw tokens
+ * should display current "hand"
+ * should allow to put tokens on board
+ */
+
